9916171bf0370d9f087a4d675f28319e702b4b4b
wilkersoh <plgu@hotmail.com>
2019-05-09 21:33:24 +0800

    formControl

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,230 +1,109 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
+const delagationDOM = document.querySelector('.rules');
 
-let arr = [];
+const addRemoveText = (e) => {
+    const childRule = e.target.parentElement.parentElement;
+    const parentRule = e.target.parentElement.parentElement.parentElement;
 
-const readAllAgain = () => {
-    const text = document.querySelectorAll('#addedRule');
-    arr.push(text);
+    parentRule.removeChild(childRule);
+    replaceHTML(parentRule);
 }
 
-// Change Old Add New
-const addRuleText = document.querySelectorAll('#addedRule');
-
-const fn = (e) => {
-    const parent = e.target.parentNode.parentNode.parentNode;
-    const child = e.target.parentNode.parentNode;
-
-    const div = document.createElement('div');
-    div.classList.add('rule-div')
-    
+const replaceHTML = (parent) => {
     const html = `
-        <div class="addRule">
-            <input name="addRule" placeholder="insert parameter..." autocomplete="off">
-            <span id="addedRule" style="color: red;">Remove rule</span>
-        </div>
-    `;
-    const changeRule = `
-    <div id="insertRule" class="insertRule">
-        <input name="addRule" placeholder="insert parameter..." autocomplete="off">
-        <span id="addedRule">add rule</span>
-        <div class="icon">
-            <i class="fas fa-minus-circle"></i>
-        </div>
-    </div>
-    ` 
-    parent.removeChild(child);
-    div.innerHTML = html;             
-    parent.insertAdjacentElement('beforeend', div);
-    parent.insertAdjacentHTML('beforeend', changeRule);
-}
+        <div class="rule-div">
+            <div class="addRule">
+                <input name="addRule" placeholder="insert parameter..." autocomplete="off">
+                <span id="removeText" class="removeText">Remove rule</span>
+            </div>
 
-addRuleText.forEach(addText => {
-    addText.addEventListener('click', fn);
-    readAllAgain();
-})
+            <div id="insertRule" class="insertRule">
+                <input name="addRule" placeholder="insert parameter..." autocomplete="off">
+                <span id="addedRule" class="addedRule">add rule</span>
 
-
-// btnsClicked
-
-const addBtns = document.querySelectorAll('.fa-plus-circle');
-
-const btnClick = (e) => {
-    const ruleParent = document.querySelector('.rules');
-    const newElmentRule = `
-    <div class="rev-rule">
-        <div class="rule">
-            <div class="ruleNum"></div>
-            <label for="revCondition">
-                <span>If</span>
-                <select id="revCondition" class="revCondition" name="revCondition">
-                </select> of the below conditions are met
-            </label>
-
-            <div class="insert-rule">
-                <div class="rule-div">
-                    <div class="addRule">
-                        <input name="addRule" placeholder="insert parameter..." autocomplete="off">
-                        <span id="addedRule">add rule</span>
-                    </div>
-        
-                    <div class="icon">
-                        <i class="fas fa-minus-circle"></i>
-                        <!-- add one more rule  -->
-                        <i class="fas fa-plus-circle active"></i>
-                    </div>
+                <div class="icon">
+                    <i class="fas fa-minus-circle" style="cursor: pointer"></i>
                 </div>
             </div>
 
         </div>
-    </div>
     `;
-    ruleParent.insertAdjacentHTML('beforeend', newElmentRule)
+    parent.insertAdjacentHTML('beforeend', html);
+}
 
-    const numTitle = document.querySelectorAll('.ruleNum');
-    for(i= 0; i < numTitle.length; i++ ){
-        numTitle.innerHTML = `Rule ${i + 1}`
+const addMoreRule = (parent) => {
+
+    const newChild = `
+        <div class="rev-rule">
+            <div class="rule">
+                <div class="ruleNum"></div>
+                <select id="addNew" name="revType">
+                    <option>Js Developer</option>
+                    <option>Ruby Developer</option>
+                    <option>Php Developer</option>
+                </select>
+                <select id="addNewType" name="addNewType">
+                    <option>Senior</option>
+                    <option>Junior</option>
+                    <option>Entry-Level</option>
+                </select>
+
+                <div class="insert-rule">
+                    <div class="rule-div">
+                        <div class="addRule">
+                            <input name="addRule" placeholder="insert parameter..." autocomplete="off">
+                            <span id="addedRule" class="addedRule">add rule</span>
+                        </div>  
+                
+                        <div class="icon">
+                            <i class="fas fa-minus-circle"></i>
+                            <i class="fas fa-plus-circle active"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    `
+    parent.insertAdjacentHTML('beforeend', newChild);
+
+    const ruleNum = document.querySelectorAll('.ruleNum');
+    for(i = 0; i < ruleNum.length; i++){
+        ruleNum[i].innerHTML = `Rule ${i + 1}`;
     }
 }
 
-addBtns.forEach(btn => {
-    btn.addEventListener('click', btnClick);
-    // numTitle();
-})
-
-// const clickRule = (elms) => {  
-//     let arr = [];
-//     elms.forEach( dom => {
-//         const element = new RenderDom(dom); 
-//         element.init();
-//     })
-//     readElement()
-//     console.log(arr);
-// }
-
-// changeRules.forEach(rule => {
-//     rule.addEventListener('click', () => {
-//         const elemntDOM = document.querySelectorAll('.rules');
-//         clickRule(elemntDOM);
-//     })
-// })
-
-// added one more rule
-const addBtn = document.querySelectorAll('.fa-plus-circle');
-// console.log(addBtn)
-/*
-addBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const ruleNum = document.querySelectorAll('.ruleNum');
-        renderNewRule(ruleNum)
-    })
-})
-
-function renderNewRule(nums){
-    nums.forEach((num, i) => {
-        num.innerHTML = `Rule ${i + 1}`;
-    })
-    const html = `
-        <div class="rules">
-            <div class="rev-rule">
-                <div class="rule">
-                    <div class="ruleNum"></div>
-                    <select id="addNew" name="revType">
-                        
-                    </select>
-                    <select id="addNewType" name="addNewType">
-                        
-                    </select>
-
-                    <div class="insert-rule">
-                        <div class="rule-div">
-                            <div class="addRule">
-                                <input name="addRule" placeholder="insert parameter..." autocomplete="off">
-                                <span id="addedRule">add rule</span>
-                            </div>
-                    
-                            <div class="icon">
-                                <i class="fas fa-minus-circle"></i>
-                                <!-- add one more rule  -->
-                                <i class="fas fa-plus-circle active"></i>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <!-- add rule -->
-                    <div id = insertRule class="insertRule">
-                        <!-- <input type="text" name="insertNewRule" class="insert add-rule" placeholder="insert parameter..." autocomplete="off">
-                        <span>add ssrule</span> -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
-
-    const beforeNum = document.querySelector('.num');
-    beforeNum.insertAdjacentHTML('beforebegin', html);;
-
-    nums.forEach((num, i) => {
-        num.innerHTML = `Rule ${i + 1}`;
-    })
-}
-*/
-
-// Options Displays
-const condition = document.querySelector('#revCondition');
-const addNew = document.querySelector('#addNew');
-const addType = document.querySelector('#addNewType');
-
-const conditionObj = {
-    All: 'All',
-    Half: 'Half',
-    None: 'None'
-}
-
-const addNewObj = {
-    Shop: 'Shop',
-    Company: 'Company',
-    Restaurant: 'Restaurant'
-}
-
-const addTypeObj = {
-    Senior: 'Senior',
-    Junior: 'Junior',
-    Freshman: 'Freshman'
-}
-
-for(let index in conditionObj){
-    // (text, value)
-    condition.options[condition.options.length] = new Option(conditionObj[index], index);
-}
-
-for(let index in addNewObj){
-    addNew.options[addNew.options.length] = new Option(addNewObj[index], index);
-}
-
-for(let index in addTypeObj){
-    addType.options[addType.options.length] = new Option(addTypeObj[index], index);
-}
 
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
+
+delagationDOM.addEventListener('click', (e) => {
+    if(e.target.classList.contains('addedRule')){
+        addRemoveText(e);
+    }
+
+    if(e.target.classList.contains('fa-plus-circle')){
+        addMoreRule(delagationDOM);
+        // optionSelection();
+    }
+    
+}, true)
+
+// Modal open And close
+const overlay = document.getElementById('overlay');
+const modalBtn = document.querySelector('[data-modal-target]');
+const modalClose = document.querySelectorAll('[data-close-button]');
+
+modalBtn.addEventListener('click', (e) => {
+    if(e.target.matches('.btn-modal')){
         const modal = document.getElementById('modal');
         openModal(modal);
-    })
+    }
 })
 
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
+modalClose.forEach( closeElmt => {
+    closeElmt.addEventListener('click', () => {
         const modal = document.getElementById('modal');
-        closeModal(modal);
+        closeModal(modal)
     })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelector('.modal.active');
-  closeModal(modals)  
 })
 
 function openModal(modal){
@@ -239,6 +118,25 @@ function closeModal(modal){
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+// Options Displays
+/*
+const optionSelection = () => {
+    const condition = document.querySelector('#revCondition');
+    const conditionObj = {
+        All: 'All',
+        Half: 'Half',
+        None: 'None'
+    }
+    
+
+    for(i of Object.keys(conditionObj)){
+        condition.options[condition.options.length] = new Option(conditionObj[i], i)
+    }
+}
+*/
+
+// const openModalButtons = document.querySelectorAll('[data-modal-target]');
 
 
 
